48136aa58d16f3d83d6781f134ece1ce0fb85cb7
MatthieuScarset <m@matthieuscarset.com>
2020-06-01 12:33:32 +0200

    Initial commit

---
+++ download_csv_file_example/main.py
@@ -0,0 +1,28 @@
+# Get CSV from Url and write to local file.
+# @author Matthieu SCARSET
+# @see https://matthieuscarset.com
+import sys
+
+import requests
+
+url = 'https://www.data.gouv.fr/fr/datasets/r/4388ede9-aa01-4942-a3cf-cf88dd024d4a'
+
+def main():
+    try:
+        # Get and write data.
+        f = open('data.csv', 'w')
+        r = requests.get(url)
+        d = r.content.decode(r.encoding)
+        f.write(d)
+    except:
+      print("Unexpected error:", sys.exc_info()[0])
+      raise
+    finally:
+        # Read your file now.
+        f = open('data.csv', 'r')
+        print(f)
+        print('Done')
+
+
+if __name__ == "__main__":
+    main()
